c4f021fe55147d452fc202795a51c949b97746e1
Matthieu Coudron <mcoudron@hotmail.com>
2021-04-23 20:24:35 +0200

    programs.neovim: use new nixpkgs API

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -43,14 +43,15 @@ let
       "";
 
   moduleConfigure = {
-    packages.home-manager = {
-      start = filter (f: f != null) (map
-        (x: if x ? plugin && x.optional == true then null else (x.plugin or x))
-        cfg.plugins);
-      opt = filter (f: f != null)
-        (map (x: if x ? plugin && x.optional == true then x.plugin else null)
-          cfg.plugins);
-    };
+    beforePlugins = "";
+    # packages.home-manager = {
+    #   start = filter (f: f != null) (map
+    #     (x: if x ? plugin && x.optional == true then null else (x.plugin or x))
+    #     cfg.plugins);
+    #   opt = filter (f: f != null)
+    #     (map (x: if x ? plugin && x.optional == true then x.plugin else null)
+    #       cfg.plugins);
+    # };
     customRC = cfg.extraConfig
       + pkgs.lib.concatMapStrings pluginConfig cfg.plugins;
   };
@@ -222,12 +223,13 @@ in {
   };
 
   config = let
+    mergedConfigure = cfg.configure // moduleConfigure;
     neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
       inherit (cfg)
         extraPython3Packages withPython3 withNodeJs withRuby viAlias vimAlias;
-      configure = cfg.configure // moduleConfigure;
-      plugins = cfg.plugins;
+      configure = mergedConfigure;
       customRC = cfg.extraConfig;
+      plugins = cfg.plugins;
     };
 
   in mkIf cfg.enable {
@@ -242,7 +244,9 @@ in {
 
     home.packages = [ cfg.finalPackage ];
 
-    xdg.configFile."nvim/init.vim".text = neovimConfig.neovimRcContent;
+    xdg.configFile = mkIf (neovimConfig.neovimRcContent != "") {
+      "nvim/init.vim".text = neovimConfig.neovimRcContent;
+    };
     programs.neovim.finalPackage = pkgs.wrapNeovimUnstable cfg.package
       (neovimConfig // {
         wrapperArgs = (lib.escapeShellArgs neovimConfig.wrapperArgs) + " "
